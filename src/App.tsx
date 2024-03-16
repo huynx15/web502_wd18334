059fe9621b0cc1312c325dd3bac7f01cf6ccf93f
huynx15 <huynx15@fpt.edu.vn>
2024-03-16 17:03:15 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// props
+// element: thuộc tính
+// function component: tham số
+
+
+function Button1({ str_button }: { str_button: string }) {
+  return (
+    <button id="id_btn" className="cl_btn">{str_button}</button>
+  );
+}
+
+function Button2({ str_button, class_button }: { str_button: string, class_button: string }) {
+  return (
+    <button id="id_btn" className={class_button}>{str_button}</button>
+  );
+}
+interface MyButton {
+  str_button: string;
+  class_button: string;
+}
+function Button3({ str_button, class_button }: MyButton) {
+  return (
+    <button id="id_btn" className={class_button}>{str_button}</button>
+  );
+}
+
+interface Student {
+  id: string;
+  name: string;
+}
+function ListStudent({ students }: { students: Student[] }) {
+  return (
+    <ul>
+      {students.map(st =>
+      (<li key={st.id}>
+        <h3>{st.name}</h3>
+        <p>{st.id}</p>
+      </li>)
+      )}
+    </ul>
+  );
+}
+
 function App() {
+  const sts = [
+    { id: "PH123456", name: "Nguyễn Xuân Huy" },
+    { id: "PH126789", name: "Nguyễn Xuân Huy" },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React - Typescript</h1>
+      <Button1 str_button="Button 1" />
+      <Button2 str_button="Button 2" class_button="cl_button" />
+      <Button3 str_button="Button 3" class_button="cl_button" />
+      <ListStudent students={sts} />
     </div>
   );
 }
